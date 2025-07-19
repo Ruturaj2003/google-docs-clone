e6ed982da5da25544dc6265b7dd1a516fd2ea749
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-07-19 19:02:52 +0530

    Implement Redo and Print

--- src/app/documents/[documentId]/toolbal.tsx
+++ src/app/documents/[documentId]/toolbal.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { LucideIcon, Undo2Icon } from "lucide-react";
+import {
+  LucideIcon,
+  PrinterIcon,
+  Redo2Icon,
+  SpellCheckIcon,
+  Undo2Icon,
+} from "lucide-react";
 import { useEditorStore } from "./use-editor-store";
 
 interface ToolbarButtonProps {
@@ -43,12 +49,39 @@ export const Toolbar = () => {
           editor?.chain().focus().undo().run();
         },
       },
+      {
+        label: "Redo",
+        icon: Redo2Icon,
+        onClick: () => {
+          editor?.chain().focus().redo().run();
+        },
+      },
+      {
+        label: "Print",
+        icon: PrinterIcon,
+        onClick: () => {
+          window.print();
+        },
+      },
+      {
+        // Not working
+        label: "Spell Check",
+        icon: SpellCheckIcon,
+        onClick: () => {
+          const current = editor?.view.dom.getAttribute("spellcheck");
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current == "false" ? "true" : "false"
+          );
+          console.log(current);
+        },
+      },
     ],
   ];
 
   return (
     <>
-      <div className="bg-[#F1f4f8] overflow-x-auto px-2.5 py-0.5 rounded-xl min-h-[48px] flex items-center gap-x-0.5 shadow-sm  ">
+      <div className=" bg-[#F1f4f8] overflow-x-auto px-2.5 py-0.5 rounded-xl min-h-[48px] flex items-center gap-x-0.5 shadow-sm  ">
         {sections[0].map((item) => {
           return <ToolbarButton key={item.label} {...item}></ToolbarButton>;
         })}
